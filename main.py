be04a24506fd68e4f791ea2dcb0242f78a4198c2
DanielSDVG <danielsdvg@gmail.com>
2021-10-27 18:35:39 +0200

    agregado requirements.txt

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+# Ejecutar con:
+# python main.py runserver -h localhost -p 8080
+
+from flask_script import Manager
 from app.app import app
+
+manager = Manager(app)
 
 # Asegurarnos de que se carga el debugger
 app.config["DEBUG"] = True 
 
 if __name__ == "__main__":
-	app.run("localhost", 8080)
+	manager.run()
