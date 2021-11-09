04ab10a1a69e1d02ea2dab79b98875c735fa4e2b
DanielSDVG <danielsdvg@gmail.com>
2021-11-09 15:12:43 +0100

    creado entorno virtual y añadido CORS

--- main.py
+++ main.py
@@ -2,9 +2,11 @@
 # python main.py runserver -h localhost -p 8080
 
 from flask_script import Manager
+from flask_cors import CORS
 from app.app import app
 
 manager = Manager(app)
+CORS(app)
 
 # Asegurarnos de que se carga el debugger
 app.config["DEBUG"] = True 
