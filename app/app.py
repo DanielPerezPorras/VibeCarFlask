9e15010a219c112f74e7d2f1a18fe82451730e70
DanielSDVG <danielsdvg@gmail.com>
2021-11-10 09:38:25 +0100

    renombrado user.py a usuario.py

--- app/app.py
+++ app/app.py
@@ -4,6 +4,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# Importamos los manejadores del sitio web
 from .routes.test import *
-from .rest.user import *
+
+# Importamos la API REST
+from .rest.usuario import *
 from .rest.trayecto import *
