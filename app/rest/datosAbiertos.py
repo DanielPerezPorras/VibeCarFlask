761ca57fe99a5c27d3672d577a03df3c66d18c47
manuleivaUma <manuleiva123@uma.es>
2021-11-27 14:56:15 +0100

    Actualizacion insomnia,api y search fusionado
    Falta editar yaml de search y arreglar la búsqueda de provincia.

--- app/rest/datosAbiertos.py
+++ app/rest/datosAbiertos.py
@@ -24,55 +24,68 @@ def getAparcamientos():
 
 @app.route("/api/v1/incidencias/search", methods=['GET'])
 def getIncidencias():
+
     global datos_incidencias
-
-    localidad = request.args['localidad']
-    print(localidad)
-    if len(datos_incidencias)==0:
-        response = urlopen(incidencias_url)
-        data = response.read()
-        json_data = geojson.loads(data)
-        datos_incidencias=json_data
-
-    datos = []
-
-    for feature in datos_incidencias["features"]:
-        print(feature["properties"]["poblacion"]) 
-        if localidad.lower() == feature["properties"]["poblacion"].lower():
-                
-            datos.append(feature)
-    
-    if len(datos)==0:
-        return jsonify({'msg' : 'La localidad buscada no contiene incidencias o no existe'})
-    else:
-        return jsonify(datos)
-
-@app.route("/api/v1/incidenciasprov/", methods=['GET'])
-def getControles():
     global datos_incidenciasprovincia
-    provincia = request.args["provincia"]
 
-    if len(datos_incidenciasprovincia)==0:
-        response = urlopen(incidencias_url)
-        data = response.read()
-        print(data)
-        json_data = geojson.loads(data)
-        datos_incidenciasprovincia=json_data
 
-    carreteras = []
+    try:
+        provincia = request.args['provincia']
+    except:
+        provincia = None
 
-    for lugar in datos_incidenciasprovincia["features"]:
-        if provincia.lower() == lugar["properties"]["provincia"].lower():     
-             carreteras.append(lugar["properties"]["carretera"])
-             carreteras.append(lugar["properties"]["sentido"])
-             carreteras.append(lugar["properties"]["causa"])
+    try:    
+        localidad = request.args['localidad']
+    except:
+        localidad = None
 
-    if(len(carreteras) == 0):
-        return jsonify("La provincia no es correcta o ninguna carretera tiene incidencias")
+    if (localidad is not None):
+        print(localidad)
+        if len(datos_incidencias)==0:
+            response = urlopen(incidencias_url)
+            data = response.read()
+            json_data = geojson.loads(data)
+            datos_incidencias=json_data
+
+        datos = []
+
+        for feature in datos_incidencias["features"]:
+            print(feature["properties"]["poblacion"]) 
+            if localidad.lower() == feature["properties"]["poblacion"].lower():
+                    
+                datos.append(feature)
+        
+        if len(datos)==0:
+            return jsonify({'msg' : 'La localidad buscada no contiene incidencias o no existe'})
+        else:
+            return jsonify(datos)
+    elif(provincia is not None):
+        if len(datos_incidenciasprovincia)==0:
+            response = urlopen(incidencias_url)
+            data = response.read()
+            print(data)
+            json_data = geojson.loads(data)
+            datos_incidenciasprovincia=json_data
+
+        carreteras = []
+
+        for lugar in datos_incidenciasprovincia["features"]:
+            if provincia.lower() == lugar["properties"]["provincia"].lower():     
+                carreteras.append(lugar["properties"]["carretera"])
+                carreteras.append(lugar["properties"]["sentido"])
+                carreteras.append(lugar["properties"]["causa"])
+
+        if(len(carreteras) == 0):
+            return jsonify({'msg' : 'La provincia no es correcta o ninguna carretera tiene incidencias'})
+        else:
+            return jsonify(carreteras)
     else:
-        return jsonify(carreteras)
+        return jsonify({'msg' : 'No se han introducido parametros de búsqueda'})
 
 
+# @app.route("/api/v1/incidenciasprov/", methods=['GET'])
+# def getProvincias():
+    
 @app.route("/api/v1/incidencias", methods=['GET'])
 def getAllIncidencias():
     global datos_AllIncidencias
@@ -82,7 +95,7 @@ def getAllIncidencias():
         data = response.read()
         print(data)
         json_data = geojson.loads(data)
-        datos_incidenciasprovincia=json_data
+        datos_AllIncidencias=json_data
     
-    return jsonify(datos_incidenciasprovincia)
+    return jsonify(datos_AllIncidencias)
 
