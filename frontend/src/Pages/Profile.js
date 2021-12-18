ca71a9aa1b0350fc59e8c98b342af3248c0a9673
GuillermoAguado <guilleam@uma.es>
2021-12-18 22:19:43 +0100

    👨‍🎨Update Profile.js, Profile.css and defaultpfp.png
    Update the default pic because it was blurry.🔼

--- frontend/src/Pages/Profile.js
+++ frontend/src/Pages/Profile.js
@@ -12,7 +12,7 @@ export const Profile = (props) => {
     const [contrasenia, setContrasenia] = useState(usuarioActual.contrasenia)
     const [link_paypal, setLink_Paypal] = useState(usuarioActual.link_paypal)
     const [url_foto_perfil, setUrl_Foto_Perfil] = useState(usuarioActual.url_foto_perfil)
-    const [rol, setRol] = useState('')
+    const [rol, setRol] = useState(usuarioActual.rol)
     const [editing, setEditing] = useState(false)
     
     return (
@@ -26,6 +26,15 @@ export const Profile = (props) => {
                     />
                     <span className='profile-name'>{`Perfil de ${nombre} ${apellidos}`}</span>
                     <span className='profile-email'>{`${email}`}</span>
+                    <div className='profile-number'><i class="bi bi-telephone icono-movil"></i><div>{telefono}</div></div>
+                    <div className='profile-rol'>
+                        {rol > 1 &&
+                            <div><i class="bi bi-sunglasses">Admin</i></div>
+                        }
+                    </div>
+                    <div className='profile-paypal'>
+                        <a href={link_paypal} class="btn btn-outline-info"><i class="bi bi-paypal"></i>{`Paypal de ${nombre} ${apellidos}`}</a>
+                    </div>
                     <button className="btn btn-primary btn-block btn-edit">
                         <i class="bi bi-pencil-square"></i> Actualizar
                     </button>
