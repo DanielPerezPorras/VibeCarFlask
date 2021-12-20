588a67f03889c879b6a50006a615f68a05bacf5e
GuillermoAguado <guilleam@uma.es>
2021-12-20 19:18:50 +0100

    🤗Add Profile Actualizar falta que fetch funcione
    Update .js and .css

--- frontend/src/Pages/Profile.js
+++ frontend/src/Pages/Profile.js
@@ -15,6 +15,34 @@ export const Profile = (props) => {
     const [rol, setRol] = useState(usuarioActual.rol)
     const [editing, setEditing] = useState(false)
     
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setEditing(true);
+    }
+
+    const actualizarUsuario = async (e) => {
+        e.preventDefault();
+        setEditing(false);
+        const respuesta = await fetch(`${props.API}/api/v1/usuarios/${id}`, {
+            method: 'PUT',
+            headers:{
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify({
+                nombre: nombre,
+                apellidos: apellidos,
+                email: email,
+                telefono: telefono,
+                contrasenia: contrasenia,
+                link_paypal: link_paypal,
+                url_foto_perfil: url_foto_perfil,
+                rol: rol
+            })
+        })
+        const data = await respuesta.json();
+        console.log(data)
+    }
+
     return (
         <div className='container-full'>
             <div className="content container-left">
@@ -24,6 +52,8 @@ export const Profile = (props) => {
                             alt={`Imagen de ${nombre} ${apellidos}`}
                             src={url_foto_perfil}
                     />
+                {!editing &&
+                    <>
                     <span className='profile-name'>{`Perfil de ${nombre} ${apellidos}`}</span>
                     <span className='profile-email'>{`${email}`}</span>
                     <div className='profile-number'><i class="bi bi-telephone icono-movil"></i><div>{telefono}</div></div>
@@ -35,13 +65,83 @@ export const Profile = (props) => {
                     <div className='profile-paypal'>
                         <a href={link_paypal} class="btn btn-outline-info"><i class="bi bi-paypal"></i>{`Paypal de ${nombre} ${apellidos}`}</a>
                     </div>
-                    <button className="btn btn-primary btn-block btn-edit">
-                        <i class="bi bi-pencil-square"></i> Actualizar
-                    </button>
+                    <form onSubmit={handleSubmit}>
+                    
+                        <button className="btn btn-primary btn-block btn-edit">
+                            <i class="bi bi-pencil-square"></i> Actualizar
+                        </button>
+                    
+                    </form>
+                    </>
+                }
             </div>
             <div className="content container-right">
-                {editing ? 
-                'Update' : 
+                {editing ?
+                    <div className='form-update'>
+                    <h2>Mis detalles</h2> 
+                    <form onSubmit={actualizarUsuario}>
+                        <div className="form-group mb-2">
+                            <label for="email">Correo</label>
+                            <input type="email"
+                            id="email" 
+                            onChange={e => setEmail(e.target.value)} 
+                            value={email}
+                            className="form-control"
+                            placeholder="Su correo..."
+                            autoFocus/>
+                        </div>
+                        <div className="form-group mb-2">
+                            <label for="nombre">Nombre</label>
+                            <input type="text"
+                            id="nombre" 
+                            onChange={e => setNombre(e.target.value)} 
+                            value={nombre}
+                            className="form-control"
+                            placeholder="Su nombre..."/>                        
+                        </div>
+                        <div className="form-group mb-2">
+                            <label for="apellidos">Apellidos</label>
+                            <input type="text"
+                            id="apellidos" 
+                            onChange={e => setApellidos(e.target.value)} 
+                            value={apellidos}
+                            className="form-control"
+                            placeholder="Sus apellidos..."/>  
+                        </div>
+                        <div className="form-group mb-2">
+                            <label for="telefono">Tel&eacute;fono</label>
+                            <input type="text"
+                            id="telefono" 
+                            onChange={e => setTelefono(e.target.value)} 
+                            value={telefono}
+                            className="form-control"
+                            placeholder="Su tel&eacute;fono..."/> 
+                        </div>
+                        <div className="form-group mb-2">
+                            <label for="contrasena">Contrase&ntilde;a</label>
+                            <input type="password"
+                            id="contrasena" 
+                            onChange={e => setContrasenia(e.target.value)} 
+                            value={contrasenia}
+                            className="form-control"
+                            placeholder="Su contras&ntilde;a..."/> 
+                        </div>
+                        <div className="form-group mb-2">
+                            <label for="link_paypal">Correo</label>
+                            <input type="url"
+                            id="link_paypal" 
+                            onChange={e => setLink_Paypal(e.target.value)} 
+                            value={link_paypal}
+                            className="form-control"
+                            placeholder="Introduzca su link de PayPal"/>
+                        </div>
+                        <div className="btn-actualizar mb-2">
+                            <button type="submit" className="btn btn-primary">
+                                <i class="bi bi-pencil-square"></i> Actualizar mis datos
+                            </button>
+                        </div>
+                    </form>
+                    </div> : 
                 'Create'}
             </div>
         </div>
