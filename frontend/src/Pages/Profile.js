981246591f95d7c048a1a7fd5f04e0070c80c0c2
GuillermoAguado <guilleam@uma.es>
2021-12-22 22:17:12 +0100

    🧽Clean Code

--- frontend/src/Pages/Profile.js
+++ frontend/src/Pages/Profile.js
@@ -179,7 +179,7 @@ export const Profile = (props) => {
                         </div>
                     </form>
                     </div> : 
-                'Create'}
+                <></>}
             </div>
         </div>
     )
