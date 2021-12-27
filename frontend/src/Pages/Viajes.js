aa7d0e5f7a65a87c1ab85d03d54b5b2845c3642b
DanielPerezPorras <0619853431@uma.es>
2021-12-27 11:08:47 +0100

    Arreglada pantalla de viajes

--- frontend/src/Pages/Viajes.js
+++ frontend/src/Pages/Viajes.js
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import { MapContainer, TileLayer } from "react-leaflet";
 import RoutineMachine from "../Components/RoutineMachine";
 import {format} from 'date-fns'
+import {importarFechaDeBD, importarFechaDeFormulario, parteFechaIgual} from '../Utilities/dates';
 
 import "../Styles/Mapa.css"
 import VibecarContext from "../Components/VibecarContext";
@@ -37,58 +38,68 @@ function Viajes() {
         const reservas = await fetch(`http://localhost:8080/api/v1/reservas`);
         const dataReservas = await reservas.json();
 
-        var coords = []
-        if (origen !== "" && destino !== ""){
-            let filtroRes = []
-            for (let d in data){
-                // Filtro precio
-                let filtroPrecio = true
-                if (precio !== ""){
-                    filtroPrecio = parseFloat(data[d]["precio"]) <= parseFloat(precio)
-                }
+        // Aplicar filtros
+        let filtroRes = []
+        for (let d in data) {
 
-                // Filtro plazas
-                let filtroPlaza = true
-                let plazasOcupadas = 0;
-                for (let r in dataReservas){
-                    if (dataReservas[r]["trayecto"]["_id"] === data[d]["_id"]){
-                        plazasOcupadas = plazasOcupadas + (dataReservas[r]["pasajeros"])
-                    }
-                }
-                console.log(data[d])
-                console.log("plazas ocupadas: " + String(plazasOcupadas))
-                console.log("plazas originales: " + parseInt(data[d]["plazas"]))
-                if (plazasOcupadas + (plazas === "" ? 1 : parseInt(plazas)) <= parseInt(data[d]["plazas"])){
-                    data[d]["plazas"] = parseInt(data[d]["plazas"]) - plazasOcupadas
-                } else {
-                    filtroPlaza = false
-                }
+            // Filtro precio
+            let filtroPrecio = true
+            if (precio !== "") {
+                filtroPrecio = parseFloat(data[d]["precio"]) <= parseFloat(precio)
+            }
 
-                // Filtro fecha
-                var fechaJson = Date.parse(data[d]["fecha_hora_salida"])
-                var fecha1 = new Date(fechaJson).toISOString().split("T")[0]
-                var fecha2 = (new Date(fecha)).toISOString().split("T")[0]
-                let filtroFecha = fecha1 === fecha2
-                
-                if (filtroFecha && filtroPrecio && filtroPlaza){
-                    filtroRes.push(data[d])
+            // Filtro plazas
+            let filtroPlaza = true
+            let plazasOcupadas = 0;
+            for (let r in dataReservas){
+                if (dataReservas[r]["trayecto"]["_id"] === data[d]["_id"]){
+                    plazasOcupadas = plazasOcupadas + (dataReservas[r]["pasajeros"])
                 }
             }
-            setTrayectos(filtroRes)
+
+            console.log("plazas ocupadas: " + String(plazasOcupadas))
+            console.log("plazas originales: " + parseInt(data[d]["plazas"]))
+            if (plazasOcupadas + (plazas === "" ? 1 : parseInt(plazas)) <= parseInt(data[d]["plazas"])){
+                data[d]["plazas"] = parseInt(data[d]["plazas"]) - plazasOcupadas
+            } else {
+                filtroPlaza = false
+            }
+
+            // Filtro fecha
+            let filtroFecha = true;
+            var fechaForm = importarFechaDeFormulario(fecha);
+            if (fechaForm !== null) {
+                var fechaJson = importarFechaDeBD(data[d]["fecha_hora_salida"]);
+                filtroFecha = parteFechaIgual(fechaJson, fechaForm);
+            }
+            console.log(filtroFecha + ", " + filtroPrecio + ", " + filtroPlaza);
             
+            if (filtroFecha && filtroPrecio && filtroPlaza){
+                filtroRes.push(data[d])
+            }
+        }
+        setTrayectos(filtroRes)
+
+        // Mapa
+        var coords = []
+        if (origen !== "" && destino !== ""){
             // Actualizamos el mapa
             const res1 = await fetch(`https://nominatim.openstreetmap.org/search?city=${origen}&country=Spain&format=json`)
             const data1 = await res1.json();
-            const data1Res = data1[0]
-            coords.push(parseFloat(data1Res["lat"]))
-            coords.push(parseFloat(data1Res["lon"]))
-            
             const res2 = await fetch(`https://nominatim.openstreetmap.org/search?city=${destino}&country=Spain&format=json`)
             const data2 = await res2.json();
+            const data1Res = data1[0]
             const data2Res = data2[0]
-            coords.push(parseFloat(data2Res["lat"]))
-            coords.push(parseFloat(data2Res["lon"]))
-            setCoord(coords) 
+
+            if (data1[0] !== undefined && data2[0] !== undefined) {
+                coords.push(parseFloat(data1Res["lat"]))
+                coords.push(parseFloat(data1Res["lon"]))
+                coords.push(parseFloat(data2Res["lat"]))
+                coords.push(parseFloat(data2Res["lon"]))
+                setCoord(coords) 
+            } else {
+                setCoord([])
+            }
         } else {
             setCoord([])
         }
@@ -121,122 +132,123 @@ function Viajes() {
     }
 
     return (
-        <div className="pantalla">
-            <title>Próximos viajes</title>
-            <div className="ladoIzq">
-                <div>
-                    <form className="card card-body">
-                        <div className="mb-3">
-                            <label htmlFor="campo-origen" className="form-label">Origen</label>
-                            <input
-                                id="campo-origen"
-                                type="text"
-                                onChange={e => setOrigen(e.target.value)}
-                                value={origen}
-                                autoFocus
-                                className="form-control" />
-                        </div>
-                        <div className="mb-3">
-                            <label htmlFor="campo-destino" className="form-label">Destino</label>
-                            <input
-                                id="campo-destino"
-                                type="text"
-                                onChange={e => setDestino(e.target.value)}
-                                value={destino}
-                                className="form-control" />
-                        </div>
-                        <div className="row">
-
-                            <div className="col-md-4 mb-3">
-                                <label htmlFor="campo-precio" className="form-label">Precio máximo</label>
-                                <div className="input-group">
-                                    <input
-                                        id="campo-precio"
-                                        type="number"
-                                        onChange={e => setPrecio(e.target.value)}
-                                        value={precio}
-                                        className="form-control" />
-                                    <span className="input-group-text">€</span>
-                                </div>
-                            </div>
-
-                            <div className="col-md-4 mb-3">
-                                <label htmlFor="campo-precio" className="form-label">N.º de plazas</label>
+        <>
+            <div className="row">
+                <div className="col-12 col-md-8">
+                    <div>
+                        <form className="card card-body">
+                            <div className="mb-3">
+                                <label htmlFor="campo-origen" className="form-label">Origen</label>
                                 <input
-                                    type="number"
-                                    onChange={e => setPlazas(e.target.value)}
-                                    value={plazas}
+                                    id="campo-origen"
+                                    type="text"
+                                    onChange={e => setOrigen(e.target.value)}
+                                    value={origen}
+                                    autoFocus
                                     className="form-control" />
                             </div>
-
-                            <div className="col-md-4 mb-3">
-                                <label htmlFor="campo-precio" className="form-label">Fecha de salida</label>
-                                <input className="form-control" type="date" min={new Date().toISOString().split("T")[0]} id="start"
-                                    onChange={e => setFecha(e.target.value)}
-                                    value={fecha}>
-                                </input>
+                            <div className="mb-3">
+                                <label htmlFor="campo-destino" className="form-label">Destino</label>
+                                <input
+                                    id="campo-destino"
+                                    type="text"
+                                    onChange={e => setDestino(e.target.value)}
+                                    value={destino}
+                                    className="form-control" />
                             </div>
+                            <div className="row">
 
-                        </div>
-                        <button className="btn btn-primary btn-block"
-                            onClick={handleSubmit}>
-                            Buscar trayectos
-                        </button>
-                        <button className="btn btn-warning btn-block"
-                            onClick={limpiar}>
-                            Limpiar
-                        </button>
-                    </form>
+                                <div className="col-md-4 mb-3">
+                                    <label htmlFor="campo-precio" className="form-label">Precio máximo</label>
+                                    <div className="input-group">
+                                        <input
+                                            id="campo-precio"
+                                            type="number"
+                                            onChange={e => setPrecio(e.target.value)}
+                                            value={precio}
+                                            className="form-control" />
+                                        <span className="input-group-text">€</span>
+                                    </div>
+                                </div>
+
+                                <div className="col-md-4 mb-3">
+                                    <label htmlFor="campo-precio" className="form-label">N.º de plazas</label>
+                                    <input
+                                        type="number"
+                                        onChange={e => setPlazas(e.target.value)}
+                                        value={plazas}
+                                        className="form-control" />
+                                </div>
+
+                                <div className="col-md-4 mb-3">
+                                    <label htmlFor="campo-precio" className="form-label">Fecha de salida</label>
+                                    <input className="form-control" type="date" min={new Date().toISOString().split("T")[0]} id="start"
+                                        onChange={e => setFecha(e.target.value)}
+                                        value={fecha}>
+                                    </input>
+                                </div>
+
+                            </div>
+                            <button className="btn btn-primary btn-block"
+                                onClick={handleSubmit}>
+                                Buscar trayectos
+                            </button>
+                            <button className="btn btn-warning btn-block"
+                                onClick={limpiar}>
+                                Limpiar
+                            </button>
+                        </form>
+                    </div>
+                    <br/>
+                    <div className="table-responsive">
+                        <table className="table table-striped align-middle">
+                            <thead>
+                                <tr>
+                                    <th>Conductor</th>
+                                    <th>Origen</th>
+                                    <th>Destino</th>
+                                    <th>Fecha [dia/Mes]</th>
+                                    <th>Duración</th>
+                                    <th>Plazas</th>
+                                    <th>Precio</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            {trayectos.map(t => (
+                                <tr key={t._id}>
+                                    <td><img src={t.conductor.url_foto_perfil} 
+                                                title={`Imagen de ${t.conductor.nombre}`}
+                                                alt={`Imagen de ${t.conductor.nombre}`}
+                                                width="80"
+                                                height="80"/> {t.conductor.nombre}</td>
+                                    <td>{t.origen}</td>
+                                    <td>{t.destino}</td>
+                                    <td>{isNaN(Date.parse(t.fecha_hora_salida)) ? "" : format(Date.parse(t.fecha_hora_salida), '[dd/MM] HH:MM')}</td>
+                                    <td>{t.duracion_estimada}</td>
+                                    <td>{t.plazas}</td>
+                                    <td>{t.precio}</td>
+                                    <td><button onClick={() => reservar(t._id)} className="btn btn-warning btn-sm btn-block">Reservar</button></td>
+                                </tr>
+                            ))}
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
-                <br/>
-                <div className="table-responsive">
-                    <table className="table table-striped align-middle">
-                        <thead>
-                            <tr>
-                                <th>Conductor</th>
-                                <th>Origen</th>
-                                <th>Destino</th>
-                                <th>Fecha [dia/Mes]</th>
-                                <th>Duración</th>
-                                <th>Plazas</th>
-                                <th>Precio</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        {trayectos.map(t => (
-                            <tr key={t._id}>
-                                <td><img src={t.conductor.url_foto_perfil} 
-                                            title={`Imagen de ${t.conductor.nombre}`}
-                                            alt={`Imagen de ${t.conductor.nombre}`}
-                                            width="80"
-                                            height="80"/> {t.conductor.nombre}</td>
-                                <td>{t.origen}</td>
-                                <td>{t.destino}</td>
-                                <td>{isNaN(Date.parse(t.fecha_hora_salida)) ? "" : format(Date.parse(t.fecha_hora_salida), '[dd/MM] HH:MM')}</td>
-                                <td>{t.duracion_estimada}</td>
-                                <td>{t.plazas}</td>
-                                <td>{t.precio}</td>
-                                <td><button onClick={() => reservar(t._id)} className="btn btn-warning btn-sm btn-block">Reservar</button></td>
-                            </tr>
-                        ))}
-                        </tbody>
-                    </table>
+                <div className="col-12 col-md-4">
+                    <MapContainer
+                        doubleClickZoom={false}
+                        id="mapId"
+                        zoom={12}
+                        center={[36.7213028,-4.4216366]}
+                    >
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                    <LanzaMaquina />
+                    </MapContainer>
                 </div>
             </div>
-            <div className="mapa">
-                <MapContainer
-                    doubleClickZoom={false}
-                    id="mapId"
-                    zoom={12}
-                    center={[36.7213028,-4.4216366]}
-                >
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-                <LanzaMaquina />
-                </MapContainer>
-            </div>
-        </div>
+        </>
     );
 }
 
--- frontend/src/Utilities/dates.js
+++ frontend/src/Utilities/dates.js
@@ -13,6 +13,53 @@ export function prepararFechaParaBD(fecha) {
     + "-" + meterCeros(fecha.getDate(), 2)
     + " " + meterCeros(fecha.getHours(), 2)
     + ":" + meterCeros(fecha.getMinutes(), 2)
-    + ":00.000000";
+    + ":" + meterCeros(fecha.getSeconds(), 6)
+    + ".000000";
 
+}
+
+export function importarFechaDeBD(str) {
+  const regex = /^(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})\.(\d{6})$/;
+  const matches = str.match(regex);
+  const fecha = new Date(
+    parseInt(matches[1]), parseInt(matches[2]) - 1, parseInt(matches[3]),
+    parseInt(matches[4]), parseInt(matches[5]), parseInt(matches[6]));
+  if (fecha instanceof Date && !isNaN(fecha.valueOf())) {
+    return fecha
+  } else {
+    return null;
+  }
+}
+
+export function importarFechaDeFormulario(str) {
+  const regex = /^(\d{4})-(\d{2})-(\d{2})(?:T(\d{2}):(\d{2}):(\d{2})(?:\.(\d{6})))?$/;
+  const matches = str.match(regex);
+  if (matches !== null) {
+
+    let fecha = null;
+
+    if (matches[4] !== undefined) {
+      fecha = new Date(
+        parseInt(matches[1]), parseInt(matches[2]) - 1, parseInt(matches[3]),
+        parseInt(matches[4]), parseInt(matches[5]), parseInt(matches[6]));
+    } else {
+      fecha = new Date(
+        parseInt(matches[1]), parseInt(matches[2]) - 1, parseInt(matches[3]),
+        0, 0, 0);
+    }
+    if (fecha instanceof Date && !isNaN(fecha.valueOf())) {
+      return fecha
+    } else {
+      return null;
+    }
+
+  } else {
+    return null;
+  }
+}
+
+export function parteFechaIgual(date1, date2) {
+  return date1.getFullYear() === date2.getFullYear()
+    && date1.getMonth() === date2.getMonth()
+    && date1.getDate() === date2.getDate();
 }
