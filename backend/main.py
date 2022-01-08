07d7fcbb76f3b788cbeaab774bbc87e39aefae1c
DanielPerezPorras <0619853431@uma.es>
2022-01-08 17:15:20 +0100

    eliminado flask-script debido al bug del import

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,7 @@
 # Ejecutar con:
 # python main.py runserver -h localhost -p 8080
 
-from flask_script import Manager
 from app.app import app
 
-manager = Manager(app)
-
 if __name__ == "__main__":
-	manager.run()
+	app.run(port=8080)
