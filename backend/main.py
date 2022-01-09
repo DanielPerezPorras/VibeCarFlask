9fe17350a0a6c3e6f55427a6e1c4507b4c48148f
DanielPerezPorras <0619853431@uma.es>
2022-01-09 11:01:03 +0100

    Migración a gunicorn

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,4 @@
-# Ejecutar con:
-# python main.py runserver -h localhost -p 8080
-
 from app.app import app
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", port=8080)
+	app.run(host="127.0.0.1", port=8080)
