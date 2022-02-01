d6f1f064efba7ccf4dcaa26befa0c9901b644e28
DanielPerezPorras <61873236+DanielPerezPorras@users.noreply.github.com>
2022-02-01 19:20:53 +0100

    intentando colocar client id de google en el frontend sin tenerlo público en github

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,14 +1,9 @@
-import { GoogleClientId as SecretGoogleClientId } from "./secrets";
-
 let API = process.env.REACT_APP_API;
 if (API === undefined) {
   API = 'http://localhost:8080'
 }
 
 let GoogleClientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
-if (GoogleClientId === undefined) {
-  GoogleClientId = SecretGoogleClientId
-}
 
 console.log("BACKEND DESPLEGADO EN: " + API);
 
